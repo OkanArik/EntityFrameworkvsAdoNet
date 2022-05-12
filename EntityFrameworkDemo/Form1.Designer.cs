e1f83c02de6a59f9ce064450edf4132424cd665e
Okan Arık <89224500+OkanArik@users.noreply.github.com>
2022-05-12 21:15:51 +0300

    Add files via upload

---
+++ EntityFrameworkDemo/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace EntityFrameworkDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.UpdateProduct = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtUpdateUnitPrice = new System.Windows.Forms.TextBox();
+            this.txtUpdateName = new System.Windows.Forms.TextBox();
+            this.txtUpdateStockAmount = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAddUnitPrice = new System.Windows.Forms.TextBox();
+            this.txtAddName = new System.Windows.Forms.TextBox();
+            this.txtAddStockAmount = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dgwProducts = new System.Windows.Forms.DataGridView();
+            this.lblSearch = new System.Windows.Forms.TextBox();
+            this.Serach = new System.Windows.Forms.Label();
+            this.btnGetById = new System.Windows.Forms.Button();
+            this.UpdateProduct.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(31, 12);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(86, 28);
+            this.btnRemove.TabIndex = 27;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // UpdateProduct
+            // 
+            this.UpdateProduct.Controls.Add(this.label5);
+            this.UpdateProduct.Controls.Add(this.label6);
+            this.UpdateProduct.Controls.Add(this.label7);
+            this.UpdateProduct.Controls.Add(this.txtUpdateUnitPrice);
+            this.UpdateProduct.Controls.Add(this.txtUpdateName);
+            this.UpdateProduct.Controls.Add(this.txtUpdateStockAmount);
+            this.UpdateProduct.Controls.Add(this.btnUpdate);
+            this.UpdateProduct.Location = new System.Drawing.Point(367, 218);
+            this.UpdateProduct.Name = "UpdateProduct";
+            this.UpdateProduct.Size = new System.Drawing.Size(228, 191);
+            this.UpdateProduct.TabIndex = 26;
+            this.UpdateProduct.TabStop = false;
+            this.UpdateProduct.Text = "Update a Product";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Unit Price";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 119);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Stock Amount";
+            // 
+            // txtUpdateUnitPrice
+            // 
+            this.txtUpdateUnitPrice.Location = new System.Drawing.Point(109, 74);
+            this.txtUpdateUnitPrice.Name = "txtUpdateUnitPrice";
+            this.txtUpdateUnitPrice.Size = new System.Drawing.Size(86, 22);
+            this.txtUpdateUnitPrice.TabIndex = 12;
+            // 
+            // txtUpdateName
+            // 
+            this.txtUpdateName.Location = new System.Drawing.Point(109, 35);
+            this.txtUpdateName.Name = "txtUpdateName";
+            this.txtUpdateName.Size = new System.Drawing.Size(86, 22);
+            this.txtUpdateName.TabIndex = 11;
+            // 
+            // txtUpdateStockAmount
+            // 
+            this.txtUpdateStockAmount.Location = new System.Drawing.Point(109, 113);
+            this.txtUpdateStockAmount.Name = "txtUpdateStockAmount";
+            this.txtUpdateStockAmount.Size = new System.Drawing.Size(86, 22);
+            this.txtUpdateStockAmount.TabIndex = 10;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(109, 153);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(86, 32);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 304);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Unit Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Stock Amount";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 16);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Add a product";
+            // 
+            // txtAddUnitPrice
+            // 
+            this.txtAddUnitPrice.Location = new System.Drawing.Point(154, 298);
+            this.txtAddUnitPrice.Name = "txtAddUnitPrice";
+            this.txtAddUnitPrice.Size = new System.Drawing.Size(163, 22);
+            this.txtAddUnitPrice.TabIndex = 21;
+            // 
+            // txtAddName
+            // 
+            this.txtAddName.Location = new System.Drawing.Point(154, 259);
+            this.txtAddName.Name = "txtAddName";
+            this.txtAddName.Size = new System.Drawing.Size(163, 22);
+            this.txtAddName.TabIndex = 20;
+            // 
+            // txtAddStockAmount
+            // 
+            this.txtAddStockAmount.Location = new System.Drawing.Point(154, 337);
+            this.txtAddStockAmount.Name = "txtAddStockAmount";
+            this.txtAddStockAmount.Size = new System.Drawing.Size(163, 22);
+            this.txtAddStockAmount.TabIndex = 19;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(154, 377);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(163, 32);
+            this.btnAdd.TabIndex = 18;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dgwProducts
+            // 
+            this.dgwProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProducts.Location = new System.Drawing.Point(31, 49);
+            this.dgwProducts.Name = "dgwProducts";
+            this.dgwProducts.RowHeadersWidth = 51;
+            this.dgwProducts.RowTemplate.Height = 24;
+            this.dgwProducts.Size = new System.Drawing.Size(564, 150);
+            this.dgwProducts.TabIndex = 17;
+            this.dgwProducts.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwProducts_CellClick);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.Location = new System.Drawing.Point(448, 21);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(147, 22);
+            this.lblSearch.TabIndex = 28;
+            this.lblSearch.TextChanged += new System.EventHandler(this.lblSearch_TextChanged);
+            // 
+            // Serach
+            // 
+            this.Serach.AutoSize = true;
+            this.Serach.Location = new System.Drawing.Point(373, 27);
+            this.Serach.Name = "Serach";
+            this.Serach.Size = new System.Drawing.Size(50, 16);
+            this.Serach.TabIndex = 29;
+            this.Serach.Text = "Search";
+            // 
+            // btnGetById
+            // 
+            this.btnGetById.Location = new System.Drawing.Point(135, 11);
+            this.btnGetById.Name = "btnGetById";
+            this.btnGetById.Size = new System.Drawing.Size(110, 29);
+            this.btnGetById.TabIndex = 30;
+            this.btnGetById.Text = "GetById";
+            this.btnGetById.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 450);
+            this.Controls.Add(this.btnGetById);
+            this.Controls.Add(this.Serach);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.UpdateProduct);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAddUnitPrice);
+            this.Controls.Add(this.txtAddName);
+            this.Controls.Add(this.txtAddStockAmount);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgwProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.UpdateProduct.ResumeLayout(false);
+            this.UpdateProduct.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.GroupBox UpdateProduct;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtUpdateUnitPrice;
+        private System.Windows.Forms.TextBox txtUpdateName;
+        private System.Windows.Forms.TextBox txtUpdateStockAmount;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAddUnitPrice;
+        private System.Windows.Forms.TextBox txtAddName;
+        private System.Windows.Forms.TextBox txtAddStockAmount;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgwProducts;
+        private System.Windows.Forms.TextBox lblSearch;
+        private System.Windows.Forms.Label Serach;
+        private System.Windows.Forms.Button btnGetById;
+    }
+}
+
---
+++ EntityFrameworkDemo/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ProductDal _productDal=new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                Name = txtAddName.Text,
+                UnitPrice = Convert.ToDecimal(txtAddUnitPrice.Text),
+                StockAmount = Convert.ToInt32(txtAddStockAmount.Text)
+            });
+            LoadProducts();
+            MessageBox.Show("Added");
+        }
+
+        private void LoadProducts()
+        {
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            _productDal.Update(new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value),
+                Name=txtUpdateName.Text,
+                UnitPrice=Convert.ToDecimal(txtUpdateUnitPrice.Text),
+                StockAmount=Convert.ToInt32(txtUpdateStockAmount.Text)
+            });
+            LoadProducts();
+            MessageBox.Show("Updated");
+        }
+
+        private void dgwProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtUpdateName.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString();
+            txtUpdateUnitPrice.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+            txtUpdateStockAmount.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            _productDal.Delete(new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value)
+                
+            });
+            LoadProducts();
+            MessageBox.Show("Deleted");
+        }
+
+        private void lblSearch_TextChanged(object sender, EventArgs e)
+        {
+            SearchProducts(lblSearch.Text);
+        }
+        private void SearchProducts(string key)
+        {
+            //var result = _productDal.GetAll().Where(x=> x.Name.ToLower().Contains(key));
+            var result = _productDal.GetByName(key);
+            dgwProducts.DataSource = result;
+        }
+    }
+}
